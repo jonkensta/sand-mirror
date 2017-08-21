bc6ce49380837c2c6b69eeb74d0cc3c47cb8269a
Jonathan Starr <jonstarr@utexas.edu>
2017-08-21 18:28:02 -0500

    Change functional structure

--- wave.py
+++ wave.py
@@ -1,12 +1,135 @@
 from __future__ import division
 
 import argparse
+from collections import namedtuple
 
 import numpy as np
-import scipy.optimize
+import scipy.misc
+import scipy.ndimage as ndimage
+
+import matplotlib.pyplot as plt
 
 
-def build_parameters_parser():
+def expi(x):
+    return np.exp(1j * x)
+
+
+def build_elements(system):
+    angles = np.linspace(0, 2*np.pi, system.num_e, endpoint=False)
+    elements = system.r_a * np.exp(1j * angles)
+    return elements
+
+
+def build_samples(system):
+    limit = 1 / np.sqrt(2)
+    lhs = np.linspace(-limit, limit, system.num_s)
+    rhs = 1j * lhs
+    samples = system.r_s * (lhs + rhs.reshape(-1, 1)).flatten()
+    return samples
+
+
+def build_transfer(system):
+    samples = build_samples(system)
+    elements = build_elements(system).reshape(1, -1)
+
+    start, stop, num = system.f_low, system.f_high, system.num_f
+    frequencies = np.linspace(start, stop, num, endpoint=True)
+    wavelengths = system.c / frequencies
+    wavelengths = wavelengths.reshape(-1, 1)
+
+    transfer = []
+
+    for index in range(system.num_s * system.num_s):
+        distance = np.abs(samples[index] - elements)
+        data = expi(2*np.pi * distance / wavelengths) / distance
+        transfer.append(data)
+
+    return np.dstack(transfer)
+
+
+def calculate_weights(image, system):
+    transfer = build_transfer(system)
+
+    def real2complex(x):
+        x = x.reshape(2, system.num_f, system.num_e)
+        x = x[0] + 1j * x[1]
+        return x
+
+    def complex2real(x):
+        x = x.flatten()
+        x = np.stack((x.real, x.imag)).flatten()
+        return x
+
+    def evaluate_response(x):
+        x = real2complex(x)
+        x = x.reshape(system.num_f, system.num_e, 1)
+
+        y = (transfer * x).sum(axis=1)
+        p = (y.real*y.real + y.imag*y.imag).sum(axis=0)
+
+        return p
+
+    desired = image.flatten().reshape(1, 1, -1)
+
+    def objective(x):
+        error = desired - evaluate_response(x)
+        return (error * error).sum()
+
+    def fprime(x):
+        x = real2complex(x)[:, :, np.newaxis]
+
+        A = desired
+        Q = transfer
+        y = (Q * x).sum(axis=1, keepdims=True)
+        p = (y.real*y.real + y.imag*y.imag).sum(axis=0, keepdims=True)
+
+        sum_ = (2 * (p - A) * (Q.conj() * y)).sum(axis=2)
+
+        sum_ = sum_.flatten()
+        sum_ = np.stack((sum_.real, sum_.imag)).flatten()
+        return sum_
+
+    def fhess_p(x, p):
+        x = real2complex(x)[:, :, np.newaxis]
+        p = real2complex(p)[:, :, np.newaxis]
+
+        A = desired
+        Q = transfer
+
+        y0 = (Q * x).sum(axis=1, keepdims=True)
+        t0 = (Q.conj() * y0)
+
+        y1 = (Q * p).sum(axis=1, keepdims=True)
+        t1 = (Q.conj() * y1)
+
+        p_ = (y0.real*y0.real + y0.imag*y0.imag).sum(axis=0, keepdims=True)
+
+        lhs = 4 * (x.conj()*t1).sum(axis=(0, 1), keepdims=True) * t0
+        rhs = 2 * t1 * (p_ - A)
+        sum_ = (lhs + rhs).sum(axis=2)
+
+        return complex2real(sum_)
+
+    f = objective
+    x0 = np.random.normal(size=2*system.num_e*system.num_f)
+
+    minimizer = scipy.optimize.fmin_ncg(
+        f, x0, fprime=fprime, fhess_p=fhess_p,
+        maxiter=75, avextol=1e-4
+    )
+    minimizer = real2complex(minimizer)
+    return minimizer
+
+
+def simulate(weights, system):
+    transfer = build_transfer(system)
+    weights = weights.reshape(system.num_f, system.num_e, 1)
+    y = (transfer * weights).sum(axis=1)
+    p = (y.real*y.real + y.imag*y.imag).sum(axis=0)
+    return p.reshape(system.num_s, system.num_s)
+
+
+def build_argparser():
     parser = argparse.ArgumentParser(
         description='Parameters for optimizing and simulating Wave.'
     )
@@ -19,6 +142,13 @@ def build_parameters_parser():
     )
 
     parser.add_argument(
+        '--signal_radius',
+        type=float,
+        default=0.2,
+        help='signal radius in meters',
+    )
+
+    parser.add_argument(
         '--speed_of_sound',
         type=float,
         default=3300.0,
@@ -28,163 +158,91 @@ def build_parameters_parser():
     parser.add_argument(
         '--lowest_tone',
         type=float,
-        default=10e3,
+        default=500,
         help='lowest tone in Hertz',
-   )
+    )
 
     parser.add_argument(
         '--highest_tone',
         type=float,
-        default=15e3,
+        default=25e3,
         help='highest tone in Hertz',
     )
 
     parser.add_argument(
         '--num_elements',
         type=int,
-        default=10,
+        default=20,
         help='number of transducer elements'
     )
 
     parser.add_argument(
         '--num_samples',
         type=int,
-        default=10,
+        default=20,
         help='number of samples in each dimension'
     )
 
     parser.add_argument(
         '--num_tones',
         type=int,
-        default=30,
+        default=20,
         help='number of tones used in each transducer'
     )
 
     return parser
 
 
-def simulate(weights, params):
-
-    array_radius = params.array_radius
-
-    num_elements = params.num_elements
-    num_samples = params.num_samples
-    num_tones = params.num_tones
-
-    angles = np.linspace(0, 2*np.pi, num_elements, endpoint=False)
-    elements = array_radius * np.exp(1j * angles)
-
-    f_low, f_high = params.lowest_tone, params.highest_tone
-    frequencies = np.linspace(f_low, f_high, params.num_tones, endpoint=True)
-    wavelengths = params.speed_of_sound / frequencies
-
-    limit = 1 / np.sqrt(2)
-    lhs = np.linspace(-limit, limit, num_samples)
-    rhs = 1j * lhs
-    samples = array_radius * (lhs + rhs.reshape(-1, 1)).flatten()
-
-    distance = np.abs(samples - elements.reshape(-1, 1))
-    wavelengths = wavelengths.reshape(-1, 1, 1)
-
-    intensity = np.exp(1j * 2 * np.pi * distance / wavelengths) / distance
-    intensity *= weights.reshape(num_tones, num_elements, 1)
-    intensity = intensity.sum(axis=1)
-    intensity = (np.abs(intensity) ** 2).sum(axis=0)
-    intensity = intensity.reshape(num_samples, num_samples)
-    return intensity
+System = namedtuple(
+    'System',
+    ['num_e', 'num_f', 'num_s', 'c', 'r_a', 'r_s', 'f_high', 'f_low']
+)
 
 
-def calculate_weights(image, params):
+def build_system(args):
+    return System(
+        num_e=args.num_elements,
+        num_f=args.num_tones,
+        num_s=args.num_samples,
+        c=args.speed_of_sound,
+        r_a=args.array_radius,
+        r_s=args.signal_radius,
+        f_high=args.highest_tone,
+        f_low=args.lowest_tone
+    )
 
-    num_tones = params.num_tones
-    num_elements = params.num_elements
-    num_samples = params.num_samples
 
-    angles = np.linspace(0, 2*np.pi, num_elements, endpoint=False)
-    elements = params.array_radius * np.exp(1j * angles)
+def load_image(filepath, system):
+    image = ndimage.imread(filepath, mode='L')
 
-    limit = 1 / np.sqrt(2)
-    lhs = np.linspace(-limit, limit, num_samples)
-    rhs = 1j * lhs
-    samples = params.array_radius * (lhs + rhs.reshape(-1, 1)).flatten()
+    grid_size = (system.num_s, system.num_s)
+    image = scipy.misc.imresize(image, size=grid_size)
 
-    f_low, f_high = params.lowest_tone, params.highest_tone
-    frequencies = np.linspace(f_low, f_high, params.num_tones, endpoint=True)
-    wavelengths = params.speed_of_sound / frequencies
-    wavelengths = wavelengths.reshape(-1, 1)
+    image = image.astype(float)
+    image /= image.max()
+    image = 1 - image
 
-    transfer = []
-    for index in range(samples.size):
-        distance = np.abs(samples[index] - elements)
-        data = np.exp(1j * 2*np.pi * distance / wavelengths) / distance
-        transfer.append(data)
+    return image
 
-    def evaluate_response(x):
-        x = x.reshape(2, num_tones, num_elements)
-        x = x[0] + 1j * x[1]
-        response = []
-        for index in range(samples.size):
-            Q = transfer[index]
-            power = np.abs((Q * x).sum(axis=1) ** 2).sum()
-            response.append(power)
-        response = np.array(response)
-        return response
 
-    desired = image.flatten()
-    def objective(x):
-        error = desired - evaluate_response(x)
-        return (error * error).sum()
+def main():
+    parser = build_argparser()
+    args = parser.parse_args()
 
-    def real2complex(x):
-        x = x.reshape(2, num_tones, num_elements)
-        x = x[0] + 1j * x[1]
-        return x
+    system = build_system(args)
 
-    def complex2real(x):
-        x = x.flatten()
-        x = np.stack((x.real, x.imag)).flatten()
-        return x
+    desired = load_image('multiply.jpg', system)
+    weights = calculate_weights(desired, system)
+    actual = simulate(weights, system)
 
-    def fprime(x):
-        x = real2complex(x)
+    plt.subplot(121)
+    plt.imshow(actual, cmap='Greys')
 
-        cumsum = np.zeros(x.shape, dtype=complex)
-        for index in range(samples.size):
-            A = desired[index]
-            Q = transfer[index]
-            power = np.abs((Q * x).sum(axis=1) ** 2).sum()
-            cumsum += 2 * (power - A) * (Q.conj().T * (Q * x).sum(axis=1)).T
+    plt.subplot(122)
+    plt.imshow(desired, cmap='Greys')
 
-        cumsum = cumsum.flatten()
-        cumsum = np.stack((cumsum.real, cumsum.imag)).flatten()
-        return cumsum
+    plt.show()
 
-    def fhess_p(x, p):
-        x = real2complex(x)
-        p = real2complex(p)
 
-        cumsum = np.zeros(x.shape, dtype=complex)
-        for index in range(samples.size):
-            A = desired[index]
-            Q = transfer[index]
-
-            term_p = (Q.conj().T * (Q * p).sum(axis=1)).T
-
-            product = (Q * x).sum(axis=1)
-            term_x = (Q.conj().T * product).T
-            power = (np.abs(product) ** 2).sum()
-
-            cumsum += 4 * (x.conj() * term_p).sum() * term_x
-            cumsum += 2 * power * term_p
-            cumsum -= 2 * A * term_p
-
-        cumsum = complex2real(cumsum)
-        return cumsum
-
-    f = objective
-    x0 = np.random.normal(size=num_elements*num_tones*2)
-    minimizer = scipy.optimize.fmin_ncg(f, x0, fprime=fprime, fhess_p=fhess_p)
-
-    minimizer = minimizer.reshape(2, -1)
-    minimizer = minimizer[0] + 1j * minimizer[1]
-    return minimizer
+if __name__ == '__main__':
+    main()
